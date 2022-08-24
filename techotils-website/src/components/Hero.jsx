324f35284f58f437760729164304e8ee072be761
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2022-08-24 13:50:52 +0500

    Get Started Resolution Fix

--- techotils-website/src/components/Hero.jsx
+++ techotils-website/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import styles from '../style';
 import { discount, robot } from '../assets';
-import GetStarted from './GetStarted'
+import GetStarted from './GetStarted';
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
